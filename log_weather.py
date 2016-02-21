c6eca7e3739a1a6fceecd25fb7c9fe9731f54c6f
Steven Keyes <stevenrkeyes@gmail.com>
2016-02-21 00:16:15 -0500

    added skeleton file for a weather logger

---
+++ log_weather.py
@@ -0,0 +1,13 @@
+# log_weather.py
+# Author: Steven Keyes
+# This scripts looks up the weather report two days from now as well as the weather report today
+# and logs them in order to compare 2-day predictions with what actually happens.
+
+
+# look up today's weather report
+
+# log it
+
+# look up two day's from now report
+
+# log it
